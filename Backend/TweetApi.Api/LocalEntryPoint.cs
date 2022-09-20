c39178b10aaa1ed67c5283ced485b83cb0ca3081
Arpit Gupta <arpit4gupta4@gmail.com>
2022-09-20 20:22:31 +0530

    Added logging locally

--- Backend/TweetApi.Api/LocalEntryPoint.cs
+++ Backend/TweetApi.Api/LocalEntryPoint.cs
@@ -1,4 +1,5 @@
 namespace TweetApi.Api;
+using Microsoft.Extensions.Logging;
 
 /// <summary>
 /// The Main function can be used to run the ASP.NET Core application locally using the Kestrel webserver.
@@ -12,6 +13,13 @@ public class LocalEntryPoint
 
     public static IHostBuilder CreateHostBuilder(string[] args) =>
         Host.CreateDefaultBuilder(args)
+            .ConfigureLogging(logging =>
+            {
+                logging.AddAWSProvider();
+
+                // When you need logging below set the minimum level. Otherwise the logging framework will default to Informational for external providers.
+                logging.SetMinimumLevel(LogLevel.Debug);
+            })
             .ConfigureWebHostDefaults(webBuilder =>
             {
                 webBuilder.UseStartup<Startup>();
